44a8a1c66dbc379ff72d1896a611d1aa6baa5cb5
MacWarrior <macwarrior94@gmail.com>
2013-12-22 18:59:36 +0100

    Version 1.2

---
+++ Oxygenz-UtilitaireMaps/Form1.Designer.vb
@@ -0,0 +1,141 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.CheckBox4 = New System.Windows.Forms.CheckBox()
+        Me.SuspendLayout()
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 125)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(307, 26)
+        Me.ProgressBar1.TabIndex = 0
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(12, 12)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(86, 17)
+        Me.CheckBox1.TabIndex = 1
+        Me.CheckBox1.Text = "Maps + DBC"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(12, 35)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(59, 17)
+        Me.CheckBox2.TabIndex = 2
+        Me.CheckBox2.Text = "VMaps"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Location = New System.Drawing.Point(12, 58)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(61, 17)
+        Me.CheckBox3.TabIndex = 3
+        Me.CheckBox3.Text = "MMaps"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Enabled = False
+        Me.Button1.Location = New System.Drawing.Point(108, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(211, 63)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Lancer l'extraction"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.Location = New System.Drawing.Point(12, 154)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(307, 17)
+        Me.Label1.TabIndex = 5
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        '
+        'Label2
+        '
+        Me.Label2.Location = New System.Drawing.Point(12, 94)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(307, 17)
+        Me.Label2.TabIndex = 6
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        '
+        'CheckBox4
+        '
+        Me.CheckBox4.AutoSize = True
+        Me.CheckBox4.Location = New System.Drawing.Point(108, 81)
+        Me.CheckBox4.Name = "CheckBox4"
+        Me.CheckBox4.Size = New System.Drawing.Size(126, 17)
+        Me.CheckBox4.TabIndex = 7
+        Me.CheckBox4.Text = "Afficher les terminaux"
+        Me.CheckBox4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(331, 101)
+        Me.Controls.Add(Me.CheckBox4)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.CheckBox3)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Oxygenz - Utilitaire d'extraction V1.2"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox2 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox3 As System.Windows.Forms.CheckBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents CheckBox4 As System.Windows.Forms.CheckBox
+
+End Class
---
+++ Oxygenz-UtilitaireMaps/Form1.vb
@@ -0,0 +1,345 @@
+﻿Imports System.Threading
+
+Public Class Form1
+    Public type As String
+    Private ThreadExtraction As Thread
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        If (Me.Button1.Text.Equals("Lancer l'extraction")) Then
+            ThreadExtraction = New Thread(AddressOf Extraction)
+            ThreadExtraction.IsBackground = True
+            ThreadExtraction.Start()
+        Else
+            StopThread()
+            ThreadExtraction.Abort()
+            ChangeLabel2("L'extraction a été annulée.", Color.Red)
+        End If
+    End Sub
+
+    Private Sub bloquerFormulaire(value As Boolean)
+        Me.CheckBox1.Enabled = Not value
+        Me.CheckBox2.Enabled = Not value
+        Me.CheckBox3.Enabled = Not value
+        Me.CheckBox4.Enabled = Not value
+        If (value) Then
+            Me.Button1.Text = "Stopper l'extraction"
+        Else
+            Me.Button1.Text = "Lancer l'extraction"
+        End If
+    End Sub
+
+    Private Sub KillProcess(ByVal value As String)
+        Dim worldserver As Process() = Process.GetProcessesByName(value)
+        Dim processus As Process
+        For Each processus In worldserver
+            processus.Kill()
+        Next processus
+    End Sub
+
+    Private Sub StopThread()
+        ChangeTailleFenetre(341, 155)
+        bloquerFormulaire(False)
+        CleanUp()
+    End Sub
+
+    Private Sub Extraction()
+        Dim psi As New ProcessStartInfo()
+        Dim processus As Process
+
+        Invoke(New Action(Of Boolean)(AddressOf bloquerFormulaire), True)
+        Invoke(New Action(Of String, Color)(AddressOf ChangeLabel2), "", Color.Black)
+
+        If Me.CheckBox1.Checked.Equals(True) And (Not CountFiles("dbc", "*").Equals(GetMaxCount("dbc")) Or Not CountFiles("maps", "*").Equals(GetMaxCount("maps"))) Then ' Extraction des Maps & DBC
+            Invoke(New Action(Of Integer, Integer)(AddressOf ChangeTailleFenetre), 341, 218)
+            Invoke(New Action(Of String, Byte())(AddressOf ExtractFile), "mapextractor.exe", My.Resources.mapextractor)
+
+            psi.FileName = Application.StartupPath & "\mapextractor.exe"
+            If CheckBox4.Checked.Equals(True) Then
+                psi.WindowStyle = ProcessWindowStyle.Minimized
+            Else
+                psi.WindowStyle = ProcessWindowStyle.Hidden
+            End If
+
+            processus = Process.Start(psi)
+
+            Dim dbcTermine As Boolean
+            dbcTermine = False
+
+            If Not CountFiles("dbc", "*").Equals(GetMaxCount("dbc")) Then
+                Invoke(New Action(Of String, Color)(AddressOf ChangeLabel2), "Extraction des DBC en cours...", Color.Black)
+                While (dbcTermine.Equals(False) And Not UBound(Diagnostics.Process.GetProcessesByName("mapextractor")) < 0)
+                    Invoke(New Action(Of String)(AddressOf UpdateProgressBar), "dbc")
+                    If CountFiles("dbc", "*").Equals(GetMaxCount("dbc")) Then
+                        dbcTermine = True
+                    End If
+                    Thread.Sleep(50)
+                End While
+            End If
+
+            If Not CountFiles("dbc", "*").Equals(GetMaxCount("dbc")) Then
+                Invoke(New Action(Of String, Color)(AddressOf ChangeLabel2), "L'extraction des DBC a échoué.", Color.Red)
+                Invoke(New Action(AddressOf StopThread))
+                Exit Sub
+            End If
+
+            If Not CountFiles("maps", "*").Equals(GetMaxCount("maps")) Then
+                Invoke(New Action(Of String, Color)(AddressOf ChangeLabel2), "Extraction des Maps en cours...", Color.Black)
+                While (Not UBound(Diagnostics.Process.GetProcessesByName("mapextractor")) And Not CountFiles("maps", "*").Equals(GetMaxCount("maps")))
+                    Invoke(New Action(Of String)(AddressOf UpdateProgressBar), "maps")
+                    Thread.Sleep(50)
+                End While
+            End If
+
+            If Not CountFiles("maps", "*").Equals(GetMaxCount("maps")) Then
+                Invoke(New Action(Of String, Color)(AddressOf ChangeLabel2), "L'extraction des Maps a échoué.", Color.Red)
+                Invoke(New Action(AddressOf StopThread))
+                Exit Sub
+            End If
+
+            KillProcess("mapextractor")
+            Invoke(New Action(Of String)(AddressOf RemoveFile), "mapextractor.exe")
+        End If
+
+            If Me.CheckBox2.Checked.Equals(True) And Not CountFiles("vmaps", "*").Equals(GetMaxCount("vmaps")) Then ' Extraction des VMaps
+                Invoke(New Action(Of Integer, Integer)(AddressOf ChangeTailleFenetre), 341, 218)
+                Invoke(New Action(Of String, Byte())(AddressOf ExtractFile), "vmap4extractor.exe", My.Resources.vmap4extractor)
+                Invoke(New Action(Of String, Byte())(AddressOf ExtractFile), "vmap4assembler.exe", My.Resources.vmap4assembler)
+                Invoke(New Action(Of String, Color)(AddressOf ChangeLabel2), "Extraction des VMaps en cours...", Color.Black)
+
+                If Not CountFiles("Buildings", "*").Equals(GetMaxCount("Buildings")) Then
+                    RemoveFile("Buildings\*")
+                    psi.FileName = Application.StartupPath & "\vmap4extractor.exe"
+                If CheckBox4.Checked.Equals(True) Then
+                    psi.WindowStyle = ProcessWindowStyle.Minimized
+                Else
+                    psi.WindowStyle = ProcessWindowStyle.Hidden
+                End If
+                    processus = Process.Start(psi)
+
+                    While (Not UBound(Diagnostics.Process.GetProcessesByName("vmap4extractor")))
+                        Invoke(New Action(Of String)(AddressOf UpdateProgressBar), "Buildings")
+                        Thread.Sleep(50)
+                    End While
+                End If
+
+                If Not CountFiles("Buildings", "*").Equals(GetMaxCount("Buildings")) Then
+                    Invoke(New Action(Of String, Color)(AddressOf ChangeLabel2), "L'extraction des VMaps a échoué.", Color.Red)
+                    RemoveFolder("Buildings")
+                    Invoke(New Action(AddressOf StopThread))
+                    Exit Sub
+                End If
+
+                KillProcess("vmap4extractor")
+                Invoke(New Action(Of String)(AddressOf RemoveFile), "vmap4extractor.exe")
+
+                Invoke(New Action(Of String, Color)(AddressOf ChangeLabel2), "Génération des VMaps en cours...", Color.Black)
+                psi.FileName = Application.StartupPath & "\vmap4assembler.exe"
+                psi.Arguments = "Buildings vmaps"
+            If CheckBox4.Checked.Equals(True) Then
+                psi.WindowStyle = ProcessWindowStyle.Minimized
+            Else
+                psi.WindowStyle = ProcessWindowStyle.Hidden
+            End If
+                processus = Process.Start(psi)
+
+                While (Not UBound(Diagnostics.Process.GetProcessesByName("vmap4assembler")) And Not CountFiles("vmaps", "*").Equals(GetMaxCount("vmaps")))
+                    Invoke(New Action(Of String)(AddressOf UpdateProgressBar), "vmaps")
+                    Thread.Sleep(50)
+                End While
+
+                If Not CountFiles("vmaps", "*").Equals(GetMaxCount("vmaps")) Then
+                    Invoke(New Action(Of String, Color)(AddressOf ChangeLabel2), "La génération des VMaps a échoué.", Color.Red)
+                    Invoke(New Action(AddressOf StopThread))
+                    Exit Sub
+                End If
+
+                RemoveFolder("Buildings")
+                KillProcess("vmap4assembler")
+                Invoke(New Action(Of String)(AddressOf RemoveFile), "vmap4assembler.exe")
+            End If
+
+            If Me.CheckBox3.Checked.Equals(True) And Not CountFiles("mmaps", "*").Equals(GetMaxCount("mmaps")) Then ' Extraction des MMaps
+                Invoke(New Action(Of Integer, Integer)(AddressOf ChangeTailleFenetre), 341, 218)
+                Invoke(New Action(Of String, Byte())(AddressOf ExtractFile), "ace.dll", My.Resources.ace)
+                Invoke(New Action(Of String, Byte())(AddressOf ExtractFile), "mmaps_generator.exe", My.Resources.mmaps_generator)
+                Invoke(New Action(Of String, Color)(AddressOf ChangeLabel2), "Génération des MMaps en cours...", Color.Black)
+
+                psi.FileName = Application.StartupPath & "\mmaps_generator.exe"
+            If CheckBox4.Checked.Equals(True) Then
+                psi.WindowStyle = ProcessWindowStyle.Minimized
+            Else
+                psi.WindowStyle = ProcessWindowStyle.Hidden
+            End If
+                processus = Process.Start(psi)
+
+                While (Not UBound(Diagnostics.Process.GetProcessesByName("mmaps_generator")) And Not CountFiles("mmaps", "*").Equals(GetMaxCount("mmaps")))
+                    Invoke(New Action(Of String)(AddressOf UpdateProgressBar), "mmaps")
+                    Thread.Sleep(50)
+                End While
+
+                If Not CountFiles("mmaps", "*").Equals(GetMaxCount("mmaps")) Then
+                    Invoke(New Action(Of String, Color)(AddressOf ChangeLabel2), "La génération des MMaps a échoué.", Color.Red)
+                    Invoke(New Action(AddressOf StopThread))
+                    Exit Sub
+                End If
+
+                KillProcess("mmaps_generator")
+                Invoke(New Action(Of String)(AddressOf RemoveFile), "ace.dll")
+                Invoke(New Action(Of String)(AddressOf RemoveFile), "mmaps_generator.exe")
+            End If
+
+            Invoke(New Action(Of String, Color)(AddressOf ChangeLabel2), "Extraction terminée avec succès.", Color.Green)
+            Invoke(New Action(AddressOf StopThread))
+    End Sub
+
+    Private Function CountFiles(dossier As String, fichier As String) As Integer
+        If System.IO.Directory.Exists(Application.StartupPath & "\" & dossier) = False Then
+            System.IO.Directory.CreateDirectory(Application.StartupPath & "\" & dossier)
+        End If
+        Return My.Computer.FileSystem.GetFiles(Application.StartupPath & "\" & dossier, FileIO.SearchOption.SearchTopLevelOnly, fichier).Count
+    End Function
+
+    Private Function GetMaxCount(type As String) As Integer
+        Select Case (type)
+            Case "dbc"
+                Return 247
+            Case "maps"
+                Return 5744
+            Case "Buildings"
+                Return 7460
+            Case "vmaps"
+                Return 9876
+            Case "mmaps"
+                Return 3657
+            Case Else
+                Return 1
+        End Select
+    End Function
+
+    Private Sub UpdateProgressBar(type As String)
+        Dim maxCount, count As Integer
+        If (type.Equals("dbc") Or type.Equals("maps") Or type.Equals("Buildings") Or type.Equals("vmaps") Or type.Equals("mmaps")) Then
+            count = CountFiles(type, "*")
+            maxCount = GetMaxCount(type)
+        Else
+            count = 0
+            maxCount = 1
+        End If
+
+        ProgressBar1.Maximum = maxCount
+        ProgressBar1.Value = count
+        Label1.Text = count & " / " & maxCount & " - " & Math.Round(count / maxCount * 100, 2, MidpointRounding.AwayFromZero) & "%"
+    End Sub
+
+    Private Sub ExtractFile(name As String, ressource As Byte())
+        Try
+            My.Computer.FileSystem.WriteAllBytes(Application.StartupPath & "\" & name, ressource, False)
+        Catch ex As Exception
+        End Try
+    End Sub
+
+    Private Sub RemoveFile(file As String)
+        Try
+            Kill(Application.StartupPath & "\" & file)
+        Catch ex As Exception
+        End Try
+    End Sub
+
+    Private Sub RemoveFolder(folder As String)
+        Try
+            My.Computer.FileSystem.DeleteDirectory(Application.StartupPath & "\" & folder, FileIO.DeleteDirectoryOption.DeleteAllContents)
+        Catch ex As Exception
+        End Try
+    End Sub
+
+    Private Sub ChangeLabel2(text As String, couleur As Color)
+        Label2.ForeColor = couleur
+        Label2.Text = text
+    End Sub
+
+    Private Sub ChangeTailleFenetre(largeur As Integer, hauteur As Integer)
+        Me.Size = New System.Drawing.Size(largeur, hauteur)
+    End Sub
+
+    Private Sub CleanUp()
+        KillProcess("mapextractor")
+        KillProcess("vmap4extractor")
+        KillProcess("vmap4assembler")
+        KillProcess("mmaps_generator")
+
+        RemoveFile("mapextractor.exe")
+        RemoveFile("vmap4extractor.exe")
+        RemoveFile("vmap4assembler.exe")
+        RemoveFile("mmaps_generator.exe")
+        RemoveFile("ace.dll")
+        If CountFiles("dbc", "*").Equals(0) Then
+            RemoveFolder("dbc")
+        End If
+        If CountFiles("maps", "*").Equals(0) Then
+            RemoveFolder("maps")
+        End If
+        If CountFiles("Buildings", "*").Equals(0) Then
+            RemoveFolder("Buildings")
+        End If
+        If CountFiles("vmaps", "*").Equals(0) Then
+            RemoveFolder("vmaps")
+        End If
+        If CountFiles("mmaps", "*").Equals(0) Then
+            RemoveFolder("mmaps")
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        CleanUp()
+    End Sub
+
+    Private Sub Form1_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+        CleanUp()
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As System.Object, e As System.EventArgs) Handles CheckBox1.CheckedChanged
+        If (Me.CheckBox1.Checked.Equals(True)) Then
+            Me.Button1.Enabled = True
+        Else
+            If (Me.CheckBox2.Checked.Equals(True)) Then
+                Me.CheckBox2.Checked = False
+            End If
+            If (Me.CheckBox3.Checked.Equals(True)) Then
+                Me.CheckBox3.Checked = False
+            End If
+            Me.Button1.Enabled = False
+        End If
+    End Sub
+
+    Private Sub CheckBox2_CheckedChanged(sender As System.Object, e As System.EventArgs) Handles CheckBox2.CheckedChanged
+        If CheckBox2.Checked.Equals(True) Then
+            If CheckBox1.Checked.Equals(False) Then
+                CheckBox1.Checked = True
+            End If
+            Me.Button1.Enabled = True
+        Else
+            If CheckBox1.Checked.Equals(False) Then
+                Button1.Enabled = False
+            End If
+            If CheckBox3.Checked.Equals(True) Then
+                CheckBox3.Checked = False
+            End If
+        End If
+    End Sub
+
+    Private Sub CheckBox3_CheckedChanged(sender As System.Object, e As System.EventArgs) Handles CheckBox3.CheckedChanged
+        If CheckBox3.Checked.Equals(True) Then
+            If CheckBox1.Checked.Equals(False) Then
+                CheckBox1.Checked = True
+            End If
+            If CheckBox2.Checked.Equals(False) Then
+                CheckBox2.Checked = True
+            End If
+            Button1.Enabled = True
+        Else
+            If CheckBox1.Checked.Equals(False) Then
+                Button1.Enabled = False
+            End If
+        End If
+    End Sub
+End Class
